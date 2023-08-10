a6fa25af2ff05a6e30f74f7370ca237499eed0e3
Mustafa Yıldırım <myldrm1025@gmail.com>
2023-08-10 20:51:34 +0300

    failed at glassfish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.utils.extendsFrom
+
 plugins {
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.allopen") version "1.8.22"
@@ -15,21 +17,20 @@ val quarkusPlatformVersion: String by project
 val ktor_version: String by project
 
 dependencies {
-    implementation("io.quarkus:quarkus-rest-client")
-    implementation("io.quarkus:quarkus-rest-client-jackson")
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
     implementation("io.quarkus:quarkus-picocli")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.4")
     implementation("io.quarkus:quarkus-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("io.quarkus:quarkus-arc")
-    implementation("io.ktor:ktor-client-cio-jvm:2.3.3")
-    testImplementation("io.quarkus:quarkus-junit5")
-    implementation("io.ktor:ktor-client-core:$ktor_version")
-    implementation("io.ktor:ktor-client-cio:$ktor_version")
-    // https://mvnrepository.com/artifact/com.netflix.conductor/conductor-client
-    implementation("com.netflix.conductor:conductor-client:3.3.6")
+    //conductor
     implementation("com.netflix.conductor:conductor-common:3.3.6")
-
+    implementation("com.netflix.conductor:conductor-client:3.3.6")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.11.4")
+    implementation("com.google.guava:guava:30.0-jre")
+    implementation("cglib:cglib:3.3.0")
+    implementation("com.sun.jersey:jersey-client:1.19.4")
+    implementation("javax.ws.rs:javax.ws.rs-api:2.0.1")
+    implementation("org.glassfish.jersey.core:jersey-common:2.22.2")
 
 }
 
